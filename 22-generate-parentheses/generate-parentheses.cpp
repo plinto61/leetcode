33365c18513e5de05beea216dddaed4720325aa3
Harsh Nagda <harshpnagda@gmail.com>
2024-03-20 11:25:03 +0530

    Time: 4 ms (45.62%) | Memory: 15 MB (65.34%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> result;
-        string init = "";
-        actualFunc(0, n, result, init);
+        actualFunc(0, n, result, "");
         return result;
     }
-    void actualFunc(int open, int remain, vector<string>& result, string& s) {
+
+private:
+    void actualFunc(int open, int remain, vector<string>& result, string s) {
         if (remain == 0 && open == 0) {
             result.push_back(s);
-        }  
+        }
         if (remain > 0) {
-            string s1 = s + '(';
-            actualFunc(open+1, remain-1, result, s1);
-        }  
+            actualFunc(open + 1, remain - 1, result, s + '(');
+        }
         if (open > 0) {
-            string s1 = s + ')';
-            actualFunc(open-1, remain, result, s1);
+            actualFunc(open - 1, remain, result, s + ')');
         }
     }
 };
