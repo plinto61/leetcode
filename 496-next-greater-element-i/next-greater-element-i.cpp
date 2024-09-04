c13977adef132eb04ae25dda840c097e6fc85bd1
Harsh Nagda <harshpnagda@gmail.com>
2024-09-04 22:11:58 +0530

    Time: 4 ms (70.23%) | Memory: 12.1 MB (65.14%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n1_len = nums1.size(), n2_len = nums2.size();
+        map<int, int> mp;
+        for(unsigned int i=0; i < n2_len; i++) {
+            mp[nums2[i]] = i;
+        }
         vector<int> result;
         for(unsigned int i=0; i < n1_len; i++) {
-            int search_elem = true;
             int found_next_greatest = false;
-            for(unsigned int j=0; j < n2_len; j++) {
-                if(search_elem) {
-                    if(nums2[j] == nums1[i]) {
-                        search_elem = false;
-                    }
-                } else {
-                    if(nums2[j] > nums1[i]) {
-                        found_next_greatest = true;
-                        result.push_back(nums2[j]);
-                        break;
-                    }
+            for(unsigned int j=mp[nums1[i]]+1; j < n2_len; j++) {
+                if(nums2[j] > nums1[i]) {
+                    found_next_greatest = true;
+                    result.push_back(nums2[j]);
+                    break;
                 }
             }
             if(!found_next_greatest) {
