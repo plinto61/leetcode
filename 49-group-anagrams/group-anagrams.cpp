4c44721da5f7b6502bcc4526fa82d740698bf87a
Harsh Nagda <harshpnagda@gmail.com>
2025-03-22 12:35:23 +0530

    Time: 85 ms (5.52%) | Memory: 25 MB (66.91%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,37 +1,22 @@
 class Solution {
+
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> result;
-
-        unordered_map<string, vector<string>> track;
-
-        for(auto str:strs) {
-            string s = sort_str(str);
-            track[s].push_back(str);
-        }
-
-        for(auto& [key, value]: track) {
-            result.push_back(value);
-        }
-
-        return result;
-    }
-
-    string sort_str(string& str) {
-        //
-        int chars[26] = {0};
-
-        for(auto c:str) {
-            chars[c-'a']++;
-        }
-        
-        string s;
-
-        for(int i=0; i<26; i++) {
-            for(int j=0; j<chars[i]; j++) {
-                s += i+'a';
+        unordered_map<string, vector<string>> mp;
+        for(string s : strs) {
+            string sorted_s = s;
+            sort(sorted_s.begin(), sorted_s.end());
+            cout << sorted_s << " " << s << endl;
+            if(mp.find(sorted_s) != mp.end()) {
+                mp[sorted_s].push_back(s);
+            } else {
+                mp[sorted_s] = {s};
             }
         }
-        return s;
+        vector<vector<string>> result;
+        for(const auto& pair : mp) {
+            result.push_back(pair.second);
+        }
+        return result;
     }
 };
