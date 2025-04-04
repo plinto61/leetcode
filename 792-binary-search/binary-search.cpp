a3149fe20baabcf8a027717084a6fdd42c205e8c
Harsh Nagda <harshpnagda@gmail.com>
2025-04-04 23:36:34 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (80.50%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -4,14 +4,14 @@ public:
         int left = 0, right = nums.size() - 1;
         while (right >= left) {
             int mid = (left + right) / 2;
-            if (nums[mid] == target) {
+
+            if (nums[mid] == target)
                 return mid;
-            }
-            if (nums[mid] < target) {
+
+            if (nums[mid] < target)
                 left = mid + 1;
-            } else {
-                right = mid -1;
-            }
+            else
+                right = mid - 1;
         }
         return -1;
     }
