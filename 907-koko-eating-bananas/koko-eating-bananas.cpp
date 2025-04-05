c08465982fb0351890bd42aad947c0bff349637f
Harsh Nagda <harshpnagda@gmail.com>
2025-04-05 13:10:55 +0530

    Time: 11 ms (44.79%) | Memory: 22.8 MB (96.79%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -5,12 +5,9 @@ public:
         int k = right_k;
         while (left_k <= right_k) {
             int curr_k = (left_k + right_k) / 2;
-            // cout << "left: " << left_k << "\tright: " << right_k << endl;
-            // cout << "curr_k: " << curr_k << "\t" << endl;
-            int h_needed = 0;
+            long long h_needed = 0;
             for (auto n : piles) {
-                double curr_h = (double)n/curr_k;
-                // cout << n << ": " << curr_h << ", ";
+                long long curr_h = (n + curr_k - 1)/curr_k;
                 h_needed += ceil(curr_h);
                 if (h_needed > h) {
                     break;
@@ -24,7 +21,6 @@ public:
                 }
                 right_k = curr_k - 1;
             }
-            // cout << endl << "k: " << k << endl << endl;
         }
         return k;
     }
