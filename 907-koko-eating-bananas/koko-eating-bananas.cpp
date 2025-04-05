caaa1edcce94e149c207b3b1660a99611b49ade8
Harsh Nagda <harshpnagda@gmail.com>
2025-04-05 13:07:14 +0530

    Time: 17 ms (17.06%) | Memory: 22.9 MB (74.72%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,41 +1,31 @@
 class Solution {
-private:
-    bool getIsKValid(vector<int>& piles, int k, int h) {
-        for(int i=0; i < piles.size(); i++) {
-            h -= piles[i]/k;
-            if(piles[i]%k > 0) {
-                h--;
-            }
-            if(h < 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int max = piles[0];
-        for (int i = 0; i < piles.size(); i++) {
-            if (piles[i] > max) {
-                max = piles[i];
+        int left_k = 1, right_k = *max_element(piles.begin(), piles.end());
+        int k = right_k;
+        while (left_k <= right_k) {
+            int curr_k = (left_k + right_k) / 2;
+            // cout << "left: " << left_k << "\tright: " << right_k << endl;
+            // cout << "curr_k: " << curr_k << "\t" << endl;
+            int h_needed = 0;
+            for (auto n : piles) {
+                double curr_h = (double)n/curr_k;
+                // cout << n << ": " << curr_h << ", ";
+                h_needed += ceil(curr_h);
+                if (h_needed > h) {
+                    break;
+                }
             }
-        }
-
-        if (h == piles.size()) {
-            return max;
-        }
-
-        int low = 1, high = max, min_k = max;
-        while (low <= high) {
-            int k = low + (high-low)/2;
-            int isKValid = getIsKValid(piles, k, h);
-            if(isKValid) {
-                high = k-1;
+            if (h_needed > h) {
+                left_k = curr_k + 1;
             } else {
-                low = k+1;
+                if (curr_k < k) {
+                    k = curr_k;
+                }
+                right_k = curr_k - 1;
             }
+            // cout << endl << "k: " << k << endl << endl;
         }
-        return low;
+        return k;
     }
 };
