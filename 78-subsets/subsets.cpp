c5c257f2145d422f2e1abe451f804d6c921cce8c
Harsh Nagda <harshpnagda@gmail.com>
2025-04-08 09:59:18 +0530

    Time: 4 ms (13.16%) | Memory: 12.6 MB (32.39%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,25 +1,25 @@
 class Solution {
-    vector<vector<int>> result;
-    vector<int> subset;
 
 private:
-    void tryall(int i, vector<int>& nums) {
+    void tryall(int i, vector<int>& nums, vector<int>& subset, vector<vector<int>>& result) {
         if (i == nums.size()) {
             result.push_back(subset);
             return;
         }
 
         // don't pick
-        tryall(i + 1, nums);
+        tryall(i + 1, nums, subset, result);
 
         // pick
         subset.push_back(nums[i]);
-        tryall(i + 1, nums);
+        tryall(i + 1, nums, subset, result);
         subset.pop_back();
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        tryall(0, nums);
+        vector<vector<int>> result;
+        vector<int> subset;
+        tryall(0, nums, subset, result);
         return result;
     }
 };
