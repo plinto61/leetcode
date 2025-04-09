c24144d61d350e4abfd58bc84f9aff449488e496
Harsh Nagda <harshpnagda@gmail.com>
2025-04-09 09:55:00 +0530

    Time: 89 ms (96.99%) | Memory: 11.7 MB (36.52%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,6 +1,4 @@
 class Solution {
-    bool result = false;
-
 public:
     bool exist(vector<vector<char>>& board, string word) {
         unordered_map<char, int> count;
@@ -13,27 +11,25 @@ public:
         int m = board.size(), n = board[0].size();
         for (int x = 0; x < m; x++) {
             for (int y = 0; y < n; y++) {
-                tryall(x, y, 0, board, word);
-                if (result)
+                if (tryall(x, y, 0, board, word))
                     return true;
             }
         }
-        return result;
+        return false;
     }
-    void tryall(int x, int y, int i, vector<vector<char>>& board, string word) {
-        if (result || !isValidCoordinate(x, y, board, word[i]))
-            return;
+    bool tryall(int x, int y, int i, vector<vector<char>>& board, string word) {
+        if (!isValidCoordinate(x, y, board, word[i]))
+            return false;
         char temp = board[x][y];
         board[x][y] = '\0';
-        if (++i == word.size()) {
-            result = true;
-            return;
-        }
-        tryall(x + 1, y, i, board, word);
-        tryall(x - 1, y, i, board, word);
-        tryall(x, y + 1, i, board, word);
-        tryall(x, y - 1, i, board, word);
+        if (++i == word.size())
+            return true;
+        bool res = tryall(x + 1, y, i, board, word) ||
+                   tryall(x - 1, y, i, board, word) ||
+                   tryall(x, y + 1, i, board, word) ||
+                   tryall(x, y - 1, i, board, word);
         board[x][y] = temp;
+        return res;
     }
     bool isValidCoordinate(int x, int y, vector<vector<char>>& board, char c) {
         return x >= 0 && y >= 0 && x < board.size() && y < board[0].size() &&
