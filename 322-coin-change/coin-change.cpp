476f5c55e9caa7cf6fcbd1cb357b274b07026dde
Harsh Nagda <harshpnagda@gmail.com>
2025-05-03 16:40:32 +0530

    Time: 18 ms (83.02%) | Memory: 17.8 MB (92.12%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,26 +1,22 @@
 class Solution {
-    unordered_map<int, int> mem;
 
 public:
     int coinChange(vector<int>& coins, int amount) {
-        if (amount == 0)
-            return 0;
+        vector<int> mem(amount + 1, -1);
+        mem[0] = 0;
 
-        if (mem.find(amount) != mem.end())
-            return mem[amount];
-
-        int min = -1;
-        for (int coin : coins) {
-            if (amount >= coin) {
-                int res = 1 + coinChange(coins, amount - coin);
-
-                if (res == 0)
-                    continue;
-
-                if (min == -1 || res < min)
-                    min = res;
+        for (int curr_amount = 1; curr_amount <= amount; curr_amount++) {
+            for (int coin : coins) {
+                if (curr_amount >= coin) {
+                    int res = 1 + mem[curr_amount - coin];
+                    if (res == 0)
+                        continue;
+                    if (mem[curr_amount] == -1 || res < mem[curr_amount])
+                        mem[curr_amount] = res;
+                }
             }
+            // cout << curr_amount << "\t" << mem[curr_amount] << endl;
         }
-        return mem[amount] = min;
+        return mem[amount];
     }
 };
