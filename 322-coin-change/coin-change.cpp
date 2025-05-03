917a48be791bdac3b851333efb31c75a9da9f9bd
Harsh Nagda <harshpnagda@gmail.com>
2025-05-03 16:46:08 +0530

    Time: 16 ms (86.68%) | Memory: 17.9 MB (81.55%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,21 +2,18 @@ class Solution {
 
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> mem(amount + 1, -1);
+        vector<int> mem(amount + 1, amount + 1);
         mem[0] = 0;
 
         for (int curr_amount = 1; curr_amount <= amount; curr_amount++) {
             for (int coin : coins) {
                 if (curr_amount >= coin) {
-                    int res = 1 + mem[curr_amount - coin];
-                    if (res == 0)
-                        continue;
-                    if (mem[curr_amount] == -1 || res < mem[curr_amount])
-                        mem[curr_amount] = res;
+                    mem[curr_amount] =
+                        min(mem[curr_amount], 1 + mem[curr_amount - coin]);
                 }
             }
             // cout << curr_amount << "\t" << mem[curr_amount] << endl;
         }
-        return mem[amount];
+        return mem[amount] == amount + 1 ? -1 : mem[amount];
     }
 };
