f93726df1ab584aa191434ac04dba55d823455f0
Harsh Nagda <harshpnagda@gmail.com>
2025-05-03 18:20:36 +0530

    Time: 14 ms (33.52%) | Memory: 12.8 MB (61.44%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    unordered_map<string, bool> table;
+public:
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        cout << s << endl;
+        if(s.size() == 0) return true;
+        if(table.find(s) != table.end()) return table[s];
+        bool ans = false;
+        for(string word : wordDict) {
+            if(s.size() < word.size()) continue;
+            if(word == s.substr(0,word.size())) {
+                table[s] = wordBreak(s.substr(word.size()), wordDict);
+                ans = ans || table[s];
+            }
+        }
+        return ans;
+    }
+};
